9cc26bcc8be228cbaad869bf35511aab41aab4fe
DylanQL <angelo.quispe.l@tecsup.edu.pe>
2025-04-06 00:29:26 -0500

    Alterando tabla TimeSheetScore, borrando el campo created_at

--- System/models.py
+++ System/models.py
@@ -17,8 +17,6 @@ class Usuario(models.Model):
 class TimeSheetScore(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
     score_ts = models.IntegerField()
-    created_at = models.DateTimeField(auto_now_add=True)
-
 
 class EvaluationDetails(models.Model):
     # Responsabilidades de la posición
