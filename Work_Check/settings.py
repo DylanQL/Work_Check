e67b38ba9b0ba694a53a04efa3d8132b72e364e3
Adrian Coronel Mendoza <adrianonel78@gmail.com>
2025-04-18 18:49:03 -0500

    Configurar para entorno de producción

--- Work_Check/settings.py
+++ Work_Check/settings.py
@@ -134,7 +134,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+FORCE_SCRIPT_NAME = env('URL_PREFIX', default='')
+
+LOGIN_URL = f"{FORCE_SCRIPT_NAME}/login"
+LOGIN_REDIRECT_URL = f"{FORCE_SCRIPT_NAME}/"
+LOGOUT_REDIRECT_URL = f"{FORCE_SCRIPT_NAME}/login"
+
+
+STATIC_URL = env('STATIC_URL', default='static/')
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
